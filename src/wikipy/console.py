9685d617acc2d59782df4c520be8414b96696ee4
Zurda <m.weizman@gmail.com>
2021-12-22 14:54:53 +0000

    Add functionality to get random wiki summary
    This commit adds the requests package, and uses it along with the standard library text wrap to fetch a random page summary from wikipedia.

--- src/wikipy/console.py
+++ src/wikipy/console.py
@@ -1,10 +1,23 @@
+import textwrap
+
 import click
+import requests
 
 from . import __version__
+
+API_URL = "https://en.wikipedia.org/api/rest_v1/page/random/summary"
 
 
 @click.command()
 @click.version_option(version=__version__)
 def main():
-    """The wikipy project"""
-    click.echo("Something to log")
+    """The wikipy project."""
+    with requests.get(API_URL) as response:
+        response.raise_for_status()
+        data = response.json()
+
+    title = data["title"]
+    extract = data["extract"]
+
+    click.secho(title, fg="green")
+    click.echo(textwrap.fill(extract))
